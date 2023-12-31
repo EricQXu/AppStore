86797fbee16d4e1e1ef37804c91856c95b13ca50
ericxu <ericxuhello3@gmail.com>
2023-12-30 20:44:02 -0800

    initial commit

---
+++ app-store/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Welcome to App Store!</h1>
+<script>
+    export const prerender = true;
+</script>
+<p class="text-green-400">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
